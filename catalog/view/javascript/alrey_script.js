04a17d2724a8c67f5a126d3caf18c218a949b037
ali97rey-desktop <alisamie97@gmail.com>
2021-02-08 11:26:04 +0330

    made live search to get search term on keyup after 500 miliseconds

--- catalog/view/javascript/alrey_script.js
+++ catalog/view/javascript/alrey_script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-
+    var timeout;
+    $('#search input').on('keyup', function () {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(function () {
+            search_term = $('#search input').val();
+            console.log(search_term);
+        }, 500);
+    });
 });
