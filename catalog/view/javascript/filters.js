088c21b7d31bd445c7961b75e69a5245d481569d
ali97rey-desktop <alisamie97@gmail.com>
2021-02-14 12:16:10 +0330

    js can get filters from url and also form checkboxses

--- catalog/view/javascript/filters.js
+++ catalog/view/javascript/filters.js
@@ -1,28 +1,40 @@
 $(document).ready(function () {
+    //store ilters in array
+    filters_array = [];
+    //get full url
+    url = new URL(location.href);
+    //get search parameters
+    search_params = new URLSearchParams(location.search);
+
+    //if filter in url exists
+    if(search_params.has('filter')){
+        //get filters from url as string
+        filters_url = search_params.get('filter');
+        //split filters from string to filters array
+        filters_array = filters_url.split(',');
+    }
 
     $('input.filter-checkbox').change(function () {
+        //get filter id when input status changes
         filter = $(this).data('filter-id');
+        //make it string
+        // filter = toString(filter);
+        console.log(typeof filter);
 
-        //get search part of url with 'window.location.search'
-        searchParams = new URLSearchParams(window.location.search)
+        console.log('changed filter: '+filter);
 
-        console.log(searchParams);
-        //if filter param exists
-        // if(searchParams.has('filter')){
-        //     url_filter = searchParams.get('filter')
-        //     alert(url_filter)
-        // }else{
-        //
-        // }
-        // url_filter = getParameterByName('filter');
-        //
-        // if(url_filter){
-        //     url_filter+=','+filter;
-        // }
-        //
-        // window.history.replaceState(null, null, window.location.href+"&filter="+filter);
+        if(filters_array.includes(filter)){
+            //delete this filter
+            filters_array.splice( $.inArray(filter,filters_array) ,1 );
+            console.log('  filter: '+filter + ' removed');
+        }else{
+            //add this filter
+            filters_array.push(filter);
+            console.log('  filter: '+filter + ' added');
+        }
 
-
+        //now we have our filter ready for ajax and url
+        console.log(filters_array)
     });
 
 });
