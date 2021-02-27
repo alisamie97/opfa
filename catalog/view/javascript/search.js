594d10a7eff67aa4dc90ac807d4c39e9238bd8d9
ali97rey-desktop <alisamie97@gmail.com>
2021-02-27 16:46:52 +0330

    ajax live search is better now

--- catalog/view/javascript/search.js
+++ catalog/view/javascript/search.js
@@ -3,8 +3,13 @@ $(document).ready(function () {
     origin = window.location.href;
     //initiate a global timeout
     timeout = 0;
-    //make event listener
-    $('#search input').on('keyup', function () {
+
+    //select result list
+    ajax_search_result_list = $('#ajax-search-result-list');
+
+    //make event listener after typing is finished
+    $('#search input').on('keyup', function (e) {
+
         //reset timeout on each event call
         if (timeout) {
             clearTimeout(timeout);
@@ -13,59 +18,47 @@ $(document).ready(function () {
         timeout = setTimeout(function () {
             //get search term from input
             search_term = $('#search input').val();
-            //select result list
-            ajax_search_result_list = $('#ajax-search-result-list');
-            product_list = $('#ajax-search-products-list');
-            category_list = $('#ajax-search-categories-list');
-            manufacturer_list = $('#ajax-search-manufacturers-list');
-            not_found = $('#search-not-found');
+
             //if search term not empty start ajax
             if (search_term.length) {
                 $.ajax({
                     type: 'get',
                     //send ajax request to live_search method
                     url: '?route=common/search/live_search&search_term=' + search_term,
-                    beforeSend: function () {
-                        // clear any child of result list
-                        ajax_search_result_list.hide();
-                        product_list.hide();
-                        category_list.hide();
-                        manufacturer_list.hide();
-                        not_found.hide();
-                    },
-                    fail: function () {
-
-                    },
                     success: function (data) {
-                        console.log(data);
-                        tempd= data;
-                        console.log(tempd);
-
-                        if($('#search-not-found',data).length > 0){
-                            not_found.show()
-                        }else{
-                            if($('#ajax-search-products-list',data).length > 0){
-                                product_list.html($('#ajax-search-products-list',data).html());
-                            }
-
-                            if($('#ajax-search-categories-list',data).length > 0){
-                                category_list.html($('#ajax-search-categories-list',data).html());
-                            }
-
-                            if($('#ajax-search-manufacturers-list',data).length > 0){
-                                manufacturer_list.html($('#ajax-search-manufacturers-list',data).html());
-                            }
-                        }
-                        ajax_search_result_list.html($(data).html());
-                        ajax_search_result_list.show();
+                        ajax_search_result_list.fadeOut(300,'swing',function (){
+                            ajax_search_result_list.html($(data).html());
+                        });
+                    },
+                    complete: function (){
+                        ajax_search_result_list.fadeIn(300,'swing');
                     }
                 });
             }else{
                 setTimeout(function (){
-                    //clear list and hide it from view after 4000 ms if search term is empty and inactive
-                    ajax_search_result_list.hide();
+                    //hide list from view after 4000 ms if search term is empty and inactive
+                    ajax_search_result_list.fadeOut(300,'swing');
                 },4000)
             }
         }, 500);
     });
+
+
+    $('#search input').on('click',function (){
+        ajax_search_result_list.fadeIn(300,'swing');
+
+    });
+
+    $(this).on('mouseup',function(e)
+    {
+        //if clicked on any where of document hide search list result
+        ajax_search_result_list = $('#ajax-search-result-list');
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!ajax_search_result_list.is(e.target) && ajax_search_result_list.has(e.target).length === 0)
+        {
+            ajax_search_result_list.fadeOut(300,'swing');
+        }
+    });
 });
+
